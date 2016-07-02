f112f41875810efdbd2c794645830b6249939d5d
BennyLEP <bendo01@gmail.com>
2016-07-02 18:49:39 +0800

    ok finish

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,12 @@ gulp.task('clean:dist', function () {
   ]);
 });
 
+gulp.task('clean:lib', function () {
+  return del([
+    'dist/js/lib/**/*'
+  ]);
+});
+
 gulp.task('copy-font-awesome', function() {return gulp.src(config.npmDir + '/font-awesome/fonts/**.*') 
   .pipe(gulp.dest('./dist/fonts')); 
 });
@@ -46,6 +52,7 @@ gulp.task('compile-scss', function () {
   .pipe(sass({includePaths: [
     'src/scss',
     config.npmDir + '/bootstrap/scss',
+    config.npmDir + '/bulma',
     config.npmDir + '/open-iconic/font/css',
     config.npmDir + '/font-awesome/scss',
     config.npmDir + '/selectize-scss/src',
@@ -61,6 +68,7 @@ gulp.task('compile-scss', function () {
   .pipe(postcss([
       require('autoprefixer')
   ]))
+  //.pipe(rename("bulma-strap.css"))
   .pipe(gulp.dest('dist/css'));
 });
 
@@ -68,9 +76,34 @@ gulp.task("compile-es6-js", function () {
   return gulp.src("src/js/es6/**/*.js")
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .pipe(concat("scripts_es6.js"))
+    .pipe(concat("bulma-strap.js"))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('copy-jquery', function() { 
+  return gulp.src(config.npmDir + '/jquery/dist/jquery.min.js') 
+  .pipe(gulp.dest('./dist/js')); 
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(["dist/css/*.css", "dist/js/*.js"], {
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+gulp.task('copy-bootstrap-tab-js', function() {
+  return gulp.src('./src/js/lib/bootstrap.tab.js') .pipe(gulp.dest('./dist/js/lib'));
+});
+
+gulp.task('copy-bootstrap-util-js', function() {
+  return gulp.src('./src/js/lib/bootstrap.util.js') .pipe(gulp.dest('./dist/js/lib'));
+});
+
+gulp.task('copy-bootstrap-dropdown-js', function() {
+  return gulp.src('./src/js/lib/bootstrap.dropdown.js') .pipe(gulp.dest('./dist/js/lib'));
 });
 
 gulp.task('sequence-copy-core', sequence(
@@ -78,8 +111,35 @@ gulp.task('sequence-copy-core', sequence(
   [
     'copy-font-awesome',
     'copy-open-iconic',
+    'copy-jquery',
     'compile-scss',
     'compile-es6-js'
   ],
+  [
+    'copy-bootstrap-tab-js',
+    'copy-bootstrap-util-js',
+    'copy-bootstrap-dropdown-js'
+  ],
   'browser-sync'
 ));
+
+gulp.task('sequence-copy-lib', sequence(
+  [
+    'copy-bootstrap-tab-js',
+    'copy-bootstrap-util-js',
+    'copy-bootstrap-dropdown-js'
+  ]
+));
+
+gulp.task('default',
+  [
+    'sequence-copy-core',
+    'sequence-copy-lib'
+  ],
+  function () {
+    gulp.watch("src/scss/**/*.scss", ['compile-scss']).on('change', browserSync.reload);
+    gulp.watch("src/js/lib/**/*.js", ['copy-bootstrap-tab-js', 'copy-bootstrap-util-js', 'copy-bootstrap-dropdown-js']).on('change', browserSync.reload);
+    gulp.watch('dist/pages/**/*.html').on('change', browserSync.reload);
+    gulp.watch('dist/**/*.html').on('change', browserSync.reload);
+  }
+);
